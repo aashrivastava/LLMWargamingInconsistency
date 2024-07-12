572e60bbd723a45803dac2876bf31ad5b1cb868d
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air-2.local>
2024-07-12 17:15:38 -0500

    starting v2 free run

--- del.py
+++ del.py
@@ -4,6 +4,7 @@ import re
 import csv
 
 perms = [
+    ('gpt-3.5-turbo', False)
     ('gpt-3.5-turbo', True),
     ('gpt-4-turbo', False),
     ('gpt-4-turbo', True)
@@ -15,14 +16,14 @@ def run_20_simuls(model, explicit_country):
     pipe = Pipeline(model, 'free', explicit_country, 20, 1.0, 'bert')
     consistencies = []
     for i in tqdm(range(20), desc='Run simulations...'):
-        response_saver = f'logging/responses/{dir_name}/run{i}'
-        chat_saver = f'logging/chats/{dir_name}/run{i}'
-        weird_saver = f'logging/weird/{dir_name}/run{i}'
+        response_saver = f'logging/responses/v2/{dir_name}/run{i}'
+        chat_saver = f'logging/chats/v2/{dir_name}/run{i}'
+        weird_saver = f'logging/weird/v2/{dir_name}/run{i}'
         consistency_move1, consistency_move2 = pipe.main(response_saver, chat_saver, weird_saver)
-        consistencies.append((consistency_move1, consistency_move1))
+        consistencies.append((consistency_move1, consistency_move2))
         print(f'Consistency move 1: {consistency_move1}')
         print(f'Consistency move 2: {consistency_move2}')
-    with open(f'logging/consistency/{dir_name}/consistency.csv', 'w') as f:
+    with open(f'logging/consistency/v2/{dir_name}/consistency.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         header = ['Move 1', 'Move 2']
         writer.writerow(header)
