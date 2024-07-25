95d09e2b220c10db65f3d8895b92c4c274a01767
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air-2.local>
2024-07-25 15:45:05 -0500

    fixed bootstrap code

--- utils/bootstrap.py
+++ utils/bootstrap.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 
 def bootstrap(dataset: list[float], n: int=1000):
@@ -13,7 +12,7 @@ def bootstrap(dataset: list[float], n: int=1000):
     for i in range(n):
         curr_dataset = []
         for j in range(len(dataset)):
-            curr_dataset.append(dataset[random.randint(0, len(dataset) - 1)])
+            curr_dataset.append(dataset[np.random.randint(0, len(dataset))])
         means.append(np.mean(curr_dataset))
     
     return means
@@ -23,11 +22,9 @@ PSEUDOCODE:
 means = new list
 for i=1,...1_000:
     curr_dataset = new list
-    for j in range 1,..., length of dataset:
-        randomly sample a value from original dataset and add to curr_dataset with replacement
+    for j =1,..., length of dataset:
+        randomly sample a value from original dataset and add to curr_dataset (do not remove from original dataset)
     calculate mean of curr_dataset
     add mean to means list
-
-plot a histogram of the values in means list
 
 '''
