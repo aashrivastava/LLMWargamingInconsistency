67bb482e163dea158eacbb3379b8f5e4465320dd
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air-2.local>
2024-08-02 15:24:59 -0500

    main plotting progress

--- utils/bootstrap.py
+++ utils/bootstrap.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def bootstrap(dataset: list[float], n: int=1000):
+def bootstrap_mean(dataset: list[float], n: int=1000):
     '''
     Given a dataset of inconsistency metrics, boostrap to get uncertainty in estimator
 
@@ -16,6 +16,23 @@ def bootstrap(dataset: list[float], n: int=1000):
         means.append(np.mean(curr_dataset))
     
     return means
+
+def bootstrap_percent_over(dataset: list[float], baseline: float, n: int=1000) -> list[float]:
+    '''
+    Given a dataset of pairs, bootstrap to get confidence on percent over/under
+
+    Input:
+        dataset: list[float]
+            collection of pairs to sample from
+    '''
+    percents_over = []
+    for i in range(n):
+        curr_dataset = np.zeros(len(dataset))
+        for j in range(len(dataset)):
+            curr_dataset[j] = dataset[np.random.randint(0, len(dataset))]
+        percents_over.append(np.sum(curr_dataset > baseline) / 15200)
+    
+    return percents_over
 
 
 
